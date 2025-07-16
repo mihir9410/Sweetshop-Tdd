443bcf03df325dacba1d7146481ff8c6d206c60c
mihir9410 <garsaniyamihir@gmail.com>
2025-07-16 21:59:40 +0530

    test: Wrote test case for retrieving all the sweets available

--- tests/SweetShop.test.js
+++ tests/SweetShop.test.js
@@ -17,3 +17,31 @@ describe('Sweetshop - Adding sweets', () => {
         expect(sweets[0].id).toBe(100);
     });
 })
+
+describe('Sweetshop - Retrieving sweets', () => {
+    it('should retrieve all sweets from the shop', () => {
+        const sweet1 = {
+            id: 101,
+            sweet_name: 'Gulab Jamun',
+            sweet_category: 'milk-based',
+            sweet_price: 30,
+            sweet_quantity: 20,
+        }
+
+        const sweet2 = {
+            id: 102,
+            sweet_name: 'Ladoo',
+            sweet_category: 'flour-based',
+            sweet_price: 25,
+            sweet_quantity: 15,
+        }
+
+        SweetShop.addSweet(sweet1);
+        SweetShop.addSweet(sweet2);
+        const sweets = SweetShop.getAllSweets();
+
+        expect(sweets.length).toBe(3); // 1 from previous test + 2 new
+        expect(sweets[1].id).toBe(101);
+        expect(sweets[2].id).toBe(102);
+    });
+})
