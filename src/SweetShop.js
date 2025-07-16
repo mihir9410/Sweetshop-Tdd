6ec28dc12dd4ce45cc436756bace99afa58b3051
mihir9410 <garsaniyamihir@gmail.com>
2025-07-16 22:16:20 +0530

    feat:  added validation to the addSweet function to pass test cases

--- src/SweetShop.js
+++ src/SweetShop.js
@@ -3,6 +3,19 @@ class SweetShop{
         this.sweets = [];
     }
     addSweet(sweet){
+        if(!sweet || !sweet.id || !sweet.sweet_name || !sweet.sweet_category || !sweet.sweet_price || !sweet.sweet_quantity){
+            throw new Error('Invalid sweet object');
+        }
+        if(sweet.sweet_price < 0 || sweet.sweet_quantity < 0){
+            throw new Error('Sweet price and quantity must be non-negative');
+        }
+        if(this.sweets.some((s) => (s.id === sweet.id) && (s.sweet_name === sweet.sweet_name) && (s.sweet_category === sweet.sweet_category) && (s.sweet_price === sweet.sweet_price))){
+            this.sweets.find((s) => s.id === sweet.id).sweet_quantity += sweet.sweet_quantity;
+            return; // If sweet already exists, just increase the quantity
+        }
+        if(this.sweets.some((s) => s.id === sweet.id)){
+            throw new Error('Sweet with this ID already exists');
+        }
         this.sweets.push(sweet);
     }
     getAllSweets(){
